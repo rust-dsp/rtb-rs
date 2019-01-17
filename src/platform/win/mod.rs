5a1bd47ee3545e211863bde8f14ac806778d29cd
Aske Simon Christensen <askesc@gmail.com>
2019-01-17 20:03:10 +0100

    Clean build on Windows and Linux

--- src/platform/win/mod.rs
+++ src/platform/win/mod.rs
@@ -1,1 +1,14 @@
 
+use crate::event::Event;
+use crate::window::WindowImpl;
+use std::ffi::c_void;
+
+pub struct PlatformWindow;
+
+impl WindowImpl for PlatformWindow {
+    fn attach(_parent: *mut c_void) -> Self {
+        PlatformWindow {}
+    }
+
+    fn add_events_hook(&mut self, _events: Box<Fn(Event)>) {}
+}
